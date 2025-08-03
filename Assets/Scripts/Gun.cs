51abac34ff4448813a01a4ddb76bff228d519710
Antony Sajan <antonysajant@gmail.com>
2025-08-03 18:47:15 +0530

    shooting fixed

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Gun: MonoBehaviour
 {
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject nozzle;
+
+
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = mousePos - (Vector2)transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (Input.GetMouseButtonDown(0))
+            Instantiate(bullet, nozzle.transform.position, nozzle.transform.rotation);
+    
     }
 }
