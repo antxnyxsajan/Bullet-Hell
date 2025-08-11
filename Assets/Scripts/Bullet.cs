04e1affb5418e8cf6d43ee65fea1dacb79ddb60c
Antony Sajan <antonysajant@gmail.com>
2025-08-11 20:05:43 +0530

    Enemy added, enemy hp,  etc

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,16 +3,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField]float speed = 20f;
+    Enemy en;
     Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         rb.linearVelocity = (mousePos - (Vector2)transform.position).normalized * speed;
+        en = FindObjectOfType<Enemy>();
     }
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            en.damaged();
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject player;
+    Vector2 movePos;
+    Rigidbody2D rb;
+    [SerializeField] int hp = 3;
+    [SerializeField] float speed = 5f;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        movePos = Vector2.MoveTowards(rb.position, player.transform.position, speed*Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(movePos);
+    }
+
+    public void damaged()
+    {
+        hp--;
+        if (hp <= 0)
+            Destroy(gameObject);
+    }
+}
