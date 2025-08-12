b2d06eba0373fd203ec0383bec98ac3f0a93849a
Antony Sajan <antonysajant@gmail.com>
2025-08-12 13:48:41 +0530

    Enemy spawn i think

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -27,3 +27,4 @@ public class Bullet : MonoBehaviour
         }
     }
 }
+ 
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] float spawnRate = 2f;
+    [SerializeField] GameObject enemy;
+    float nextSpawn = 0f;
+    Vector2 spawnPos=new Vector2();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Time.time >= nextSpawn)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            nextSpawn = Time.time + spawnRate;
+        }
+    }
+
+    
+}
